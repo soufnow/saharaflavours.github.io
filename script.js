58932726219479a61b37145428c0961e9e8ce6de
soufiane <soufianetaoufikofficial@gmail.com>
2023-04-24 21:43:33 +0100

    correct comment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -8,7 +8,6 @@ function myFunction() {
   }
 }
 
- 
 // google translate function
 function googleTranslateElementInit() {
   new google.translate.TranslateElement(
