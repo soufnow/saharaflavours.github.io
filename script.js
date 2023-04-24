be53d83a00f024d859dfaa21af94c32fddeef900
soufiane <soufianetaoufikofficial@gmail.com>
2023-04-24 21:30:09 +0100

    add javascript for star rating

--- script.js
+++ script.js
@@ -17,4 +17,20 @@ function googleTranslateElementInit() {
   );
 }
 
+// function to rate the recipe
+function rateRecipe(columnNumber, rating) {
+  // get the star rating element for the given column
+  var starRating = document.getElementById("star_rating_" + columnNumber);
 
+  // get all the stars in the rating element
+  var stars = starRating.getElementsByClassName("star");
+
+  // loop through the stars and set the class based on the rating
+  for (var i = 0; i < stars.length; i++) {
+    if (i < rating) {
+      stars[i].classList.add("rated");
+    } else {
+      stars[i].classList.remove("rated");
+    }
+  }
+}
